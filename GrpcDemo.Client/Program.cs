887f01a41e09916d294917fdad7f78376475a3c4
kun <kun@centrality.ai>
2021-04-27 11:24:35 +1200

    init grpc

---
+++ GrpcDemo.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Grpc.Net.Client;
+using GrpcDemo.Service;
+
+Console.WriteLine("Enter return to start");
+Console.ReadLine();
+
+var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(new HelloRequest { Name = "kun" });
+Console.WriteLine(response.Message);
+
+Console.ReadLine();
+
